307c1353814279e160897108a64206b790aa176b
Zhi4a <sohuga5656@gmail.com>
2023-04-10 20:08:38 +0300

    models done

---
+++ FSTR/API/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    otc = models.CharField(max_length=50)
+    phone = models.CharField(max_length=25)
+    email = models.EmailField(unique=True)
+
+
+class Pass(models.Model):
+    ADDED_STATUS = [
+        ('new', 'новое'),
+        ('pending', 'взято в работу'),
+        ('accepted', 'успешно'),
+        ('rejected', 'не принято'),
+    ]
+
+    created = models.DateTimeField(auto_now_add=True)
+    beauty_title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    other_titles = models.CharField(max_length=255)
+    connect = models.CharField(max_length=255, blank=True)
+    add_time = models.DateTimeField()
+    status = models.CharField(max_length=8, choices=ADDED_STATUS, default='new')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    coordinates = models.ForeignKey('Coordinates', on_delete=models.CASCADE)
+    levels = models.ForeignKey('Level', blank=True, on_delete=models.PROTECT)
+
+
+class Coordinates(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.IntegerField()
+
+
+class Level(models.Model):
+    winter_level = models.CharField(max_length=3, blank=True)
+    summer_level = models.CharField(max_length=3, blank=True)
+    autumn_level = models.CharField(max_length=3, blank=True)
+    spring_level = models.CharField(max_length=3, blank=True)
+
+
+class Images(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=20)
+    data = models.BinaryField()
+
+    passes = models.ForeignKey(Pass, related_name='images', on_delete=models.CASCADE)
